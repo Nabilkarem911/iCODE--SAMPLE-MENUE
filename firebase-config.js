2384987d6b30e007cded68636d7964e944656d63
Nabilkarem911 <nabilkarem@gmail.com>
2026-02-06 22:34:07 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// 1. استيراد المكتبات من سيرفرات جوجل مباشرة (CDN)
+// firebase-config.js
+// 1. استيراد المكتبات من سيرفرات جوجل (CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-// 2. بيانات مشروعك (من الكود اللي انت بعته)
+// 2. بيانات مشروعك (أنا نسختها من اللي انت بعته بالظبط)
 const firebaseConfig = {
   apiKey: "AIzaSyAHw9aNyLuZZ-_90OK0xIgRZ62SPawEthc",
   authDomain: "ai-menu-28154.firebaseapp.com",
